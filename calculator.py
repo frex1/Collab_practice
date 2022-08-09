3e1759ef87d511f53c23e849732fd250a0b70b8c
freeman.goja <michael.toryilagoja@dteenergy.com>
2022-08-09 19:29:43 -0400

    Add calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def calculator(a, b, op):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        return a / b
+    else:
+        return 'Invalid operator'
